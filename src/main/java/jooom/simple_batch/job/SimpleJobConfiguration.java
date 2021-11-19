117d8fa6126cdcf8a785a1f648ed543971022ef3
jooomOut <rimeilo324@naver.com>
2021-11-20 00:27:17 +0900

    Feat: jobParameters 추가
    paremeters가 없을 경우 jobInstance는 한 번만 실행됨, 메타테이블에도 두 번 이상 기록되지 않음

--- src/main/java/jooom/simple_batch/job/SimpleJobConfiguration.java
+++ src/main/java/jooom/simple_batch/job/SimpleJobConfiguration.java
@@ -5,10 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,16 +24,18 @@ public class SimpleJobConfiguration {
     @Bean
     public Job moveUserData() {
         return jobBuilderFactory.get("moveUserData")
-                .start(getData())
+                .start(getData(null))
                 .build();
 
     }
 
     @Bean
-    public Step getData(){
+    @JobScope
+    public Step getData(@Value("#{jobParameters[requestDate]}") String requestData){
         return stepBuilderFactory.get("getData")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>> START getData");
+                    log.info(">>>> requestDate = {}", requestData);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
