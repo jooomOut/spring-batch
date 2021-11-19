b178e184938fcf0c16571fd37eaf1bbe6d072094
jooomOut <rimeilo324@naver.com>
2021-11-19 18:17:58 +0900

    initial commit

---
+++ src/main/java/jooom/simple_batch/SimpleBatchApplication.java
@@ -0,0 +1,13 @@
+package jooom.simple_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jooom/simple_batch/SimpleBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package jooom.simple_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
