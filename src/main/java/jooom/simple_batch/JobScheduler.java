31da6f9eb3694a937a07f9ca68bdf2cf33c36eb2
jooomOut <rimeilo324@naver.com>
2021-11-20 23:21:58 +0900

    Feat: 스프링 스케쥴러 설정

---
+++ src/main/java/jooom/simple_batch/JobScheduler.java
@@ -0,0 +1,42 @@
+package jooom.simple_batch;
+
+import jooom.simple_batch.job.JpaPagingItemReaderJobConfiguration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JobScheduler {
+
+    private final JpaPagingItemReaderJobConfiguration jpaPagingItemReaderJobConfiguration;
+    private final JobLauncher jobLauncher;
+
+    @Scheduled( cron="*/30 * * * * *" ) // 30초 마다 실행됩니다.
+    public void runJob() {
+        log.info("RUN Scheduler");
+
+        Map<String, JobParameter> confMap = new HashMap<>();
+        confMap.put("version", new JobParameter(System.currentTimeMillis()));
+        JobParameters jobParameters = new JobParameters(confMap);
+
+        try {
+            jobLauncher.run(jpaPagingItemReaderJobConfiguration.jpaPagingItemReaderJob(), jobParameters);
+
+        } catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException
+                | JobParametersInvalidException | org.springframework.batch.core.repository.JobRestartException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
